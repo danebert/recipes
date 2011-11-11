2fd417026839acfad97316743f6fbefd49327df9
danebert <danebert@users.noreply.github.com>
2011-11-11 23:15:14 +0000

    Create/Edit Recipe is now restricted to Categories owned by appropriate user.

--- grails-app/controllers/com/mathin/recipes/RecipeController.groovy
+++ grails-app/controllers/com/mathin/recipes/RecipeController.groovy
@@ -2,6 +2,8 @@ package com.mathin.recipes
 
 
 class RecipeController {
+	transient springSecurityService
+
 	static scaffold = true
 
 	def list() {
@@ -9,6 +11,19 @@ class RecipeController {
 					recipeInstanceList: Recipe.list(),
 					recipeInstanceTotal: Recipe.count(),
 					categoryInstanceList: Category.list(sort:"rank", order: "asc")
+				]
+	}
+	def create() {
+		[
+					categoryInstanceList: Category.findAllByOwner(springSecurityService.currentUser)
+				]
+	}
+	def edit() {
+		def recipe = Recipe.get(params.id)
+
+		[
+					subRecipe: recipe,
+					categoryInstanceList: Category.findAllByOwner(recipe.owner)
 				]
 	}
 }
