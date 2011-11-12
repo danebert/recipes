4e1f9247e1a2b3200fbc09f8199e5783ac74c098
danebert <danebert@users.noreply.github.com>
2011-11-12 01:19:55 +0000

    restricting delete to owner for Category

--- grails-app/controllers/com/mathin/recipes/CategoryController.groovy
+++ grails-app/controllers/com/mathin/recipes/CategoryController.groovy
@@ -1,5 +1,7 @@
 package com.mathin.recipes
 
+import org.springframework.dao.DataIntegrityViolationException
+
 class CategoryController {
 	transient springSecurityService
 
@@ -16,6 +18,43 @@ class CategoryController {
 		} else {
 			flash.message = "You cannot edit because this is not your Category"
 			redirect action: show, id: category.id
+		}
+	}
+
+	def delete() {
+		def user = springSecurityService.currentUser
+
+		def categoryInstance = Category.get(params.id)
+
+		if (!categoryInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'category.label', default: 'Category'),
+				params.id
+			])
+			redirect(action: "list")
+			return
+		}
+
+		if(categoryInstance.owner.equals(user)) {
+
+			try {
+				categoryInstance.delete(flush: true)
+				flash.message = message(code: 'default.deleted.message', args: [
+					message(code: 'category.label', default: 'Category'),
+					params.id
+				])
+				redirect(action: "list")
+			}
+			catch (DataIntegrityViolationException e) {
+				flash.message = message(code: 'default.not.deleted.message', args: [
+					message(code: 'category.label', default: 'Category'),
+					params.id
+				])
+				redirect(action: "show", id: params.id)
+			}
+		} else {
+			flash.message = "You cannot delete because this is not your Category"
+			redirect action: show, id: categoryInstance.id
 		}
 	}
 }
