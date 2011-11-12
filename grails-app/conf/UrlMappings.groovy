07599103868e6ce6bffc68a3579a48288347c0cf
danebert <danebert@users.noreply.github.com>
2011-11-12 00:42:45 +0000

    moved existing index page to /status.  Replaced existing index page with cleaner links

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,10 +1,12 @@
 class UrlMappings {
 
 	static mappings = {
-		"/$controller/$action?/$id?"{ constraints { // apply constraints here
+		"/$controller/$action?/$id?"{ constraints {
+				// apply constraints here
 			} }
 
 		"/"(view:"/index")
+		"/status"(view:"/status")
 		"500"(view:'/error')
 
 		"/login/$action?"(controller: "login")
