46424417d7bd46df3b5527f14743db6a6a67e58a
danebert <danebert@204e5ef0-c04f-459e-9bd4-fea6ea9c55c1>
2011-11-12 01:24:02 +0000

    restricting delete to owner for SubCategory

--- trunk/grails-app/controllers/com/mathin/recipes/SubCategoryController.groovy
+++ trunk/grails-app/controllers/com/mathin/recipes/SubCategoryController.groovy
@@ -1,5 +1,7 @@
 package com.mathin.recipes
 
+import org.springframework.dao.DataIntegrityViolationException
+
 class SubCategoryController {
 	transient springSecurityService
 
@@ -22,6 +24,38 @@ class SubCategoryController {
 		} else {
 			flash.message = "You cannot edit because this is not your SubCategory"
 			redirect action: show, id: subCategory.id
+		}
+	}
+	def delete() {
+		def user = springSecurityService.currentUser
+		def subCategoryInstance = SubCategory.get(params.id)
+		if (!subCategoryInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'subCategory.label', default: 'SubCategory'),
+				params.id
+			])
+			redirect(action: "list")
+			return
+		}
+		if(subCategoryInstance.owner.equals(user)) {
+			try {
+				subCategoryInstance.delete(flush: true)
+				flash.message = message(code: 'default.deleted.message', args: [
+					message(code: 'subCategory.label', default: 'SubCategory'),
+					params.id
+				])
+				redirect(action: "list")
+			}
+			catch (DataIntegrityViolationException e) {
+				flash.message = message(code: 'default.not.deleted.message', args: [
+					message(code: 'subCategory.label', default: 'SubCategory'),
+					params.id
+				])
+				redirect(action: "show", id: params.id)
+			}
+		} else {
+			flash.message = "You cannot delete because this is not your SubCategory"
+			redirect action: show, id: subCategoryInstance.id
 		}
 	}
 }
