c536885788862a3bd5ff72e79686b423b0a5c991
danebert <danebert@users.noreply.github.com>
2011-11-12 01:26:50 +0000

    restricting delete to owner for Recipe

--- grails-app/controllers/com/mathin/recipes/RecipeController.groovy
+++ grails-app/controllers/com/mathin/recipes/RecipeController.groovy
@@ -1,5 +1,7 @@
 package com.mathin.recipes
 
+import org.springframework.dao.DataIntegrityViolationException
+
 
 class RecipeController {
 	transient springSecurityService
@@ -32,6 +34,40 @@ class RecipeController {
 		} else {
 			flash.message = "You cannot edit because this is not your Recipe"
 			redirect action: show, id: recipe.id
+		}
+	}
+
+	def delete() {
+		def user = springSecurityService.currentUser
+
+		def recipeInstance = Recipe.get(params.id)
+		if (!recipeInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [
+				message(code: 'recipe.label', default: 'Recipe'),
+				params.id
+			])
+			redirect(action: "list")
+			return
+		}
+		if(recipeInstance.owner.equals(user)) {
+			try {
+				recipeInstance.delete(flush: true)
+				flash.message = message(code: 'default.deleted.message', args: [
+					message(code: 'recipe.label', default: 'Recipe'),
+					params.id
+				])
+				redirect(action: "list")
+			}
+			catch (DataIntegrityViolationException e) {
+				flash.message = message(code: 'default.not.deleted.message', args: [
+					message(code: 'recipe.label', default: 'Recipe'),
+					params.id
+				])
+				redirect(action: "show", id: params.id)
+			}
+		} else {
+			flash.message = "You cannot delete because this is not your Recipe"
+			redirect action: show, id: recipeInstance.id
 		}
 	}
 }
