396c1b49553a795f53ce25d20956444a908ea0f6
danebert <danebert@users.noreply.github.com>
2011-11-13 21:48:16 +0000

    divided into prod/dev sections added creation of karin/dan users to prod section

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,62 +7,87 @@ import com.mathin.recipes.SubCategory
 
 class BootStrap {
 
-	def init = { servletContext ->
+	def init = {
+		servletContext ->
 
 		def userRole = SecRole.findByAuthority('ROLE_USER') ?: new SecRole(authority: 'ROLE_USER').save(failOnError: true)
 		def adminRole = SecRole.findByAuthority('ROLE_ADMIN') ?: new SecRole(authority: 'ROLE_ADMIN').save(failOnError: true)
+		environments {
+			production {
+				def karin = SecUser.findByUsername('karin') ?: new SecUser(
+				username: 'karin',
+				password: '6106788975',
+				displayName: 'Karin',
+				enabled: true).save(failOnError: true)
 
-		def normalUser = SecUser.findByUsername('normal') ?: new SecUser(
+				if (!karin.authorities.contains(userRole)) {
+					SecUserSecRole.create karin, userRole
+				}
+				def dan = SecUser.findByUsername('dan') ?: new SecUser(
+				username: 'dan',
+				password: '13wrens',
+				displayName: 'Dan',
+				enabled: true).save(failOnError: true)
+
+				if (!dan.authorities.contains(userRole)) {
+					SecUserSecRole.create dan, userRole
+				}
+				if (!dan.authorities.contains(adminRole)) {
+					SecUserSecRole.create dan, adminRole
+				}
+			}
+			development {
+				def normalUser = SecUser.findByUsername('normal') ?: new SecUser(
 				username: 'normal',
 				password: 'normal',
 				displayName: 'Normal',
 				enabled: true).save(failOnError: true)
 
-		if (!normalUser.authorities.contains(userRole)) {
-			SecUserSecRole.create normalUser, userRole
-		}
+				if (!normalUser.authorities.contains(userRole)) {
+					SecUserSecRole.create normalUser, userRole
+				}
 
-		def secondUser = SecUser.findByUsername('secondUser') ?: new SecUser(
+				def secondUser = SecUser.findByUsername('secondUser') ?: new SecUser(
 				username: 'secondUser',
 				password: 'secondUser',
 				displayName: 'Second User',
 				enabled: true).save(failOnError: true)
 
-		if (!secondUser.authorities.contains(userRole)) {
-			SecUserSecRole.create secondUser, userRole
-		}
+				if (!secondUser.authorities.contains(userRole)) {
+					SecUserSecRole.create secondUser, userRole
+				}
 
-		def adminUser = SecUser.findByUsername('admin') ?: new SecUser(
+				def adminUser = SecUser.findByUsername('admin') ?: new SecUser(
 				username: 'admin',
 				password: 'nimda',
 				displayName: 'Administrator',
 				enabled: true).save(failOnError: true)
 
-		if (!adminUser.authorities.contains(adminRole)) {
-			SecUserSecRole.create adminUser, adminRole
-		}
+				if (!adminUser.authorities.contains(adminRole)) {
+					SecUserSecRole.create adminUser, adminRole
+				}
 
 
-		if( !Recipe.count() ){
-			Category mainDish = new Category(name:"Main Dish", rank:2, owner:normalUser).save(failOnError: true)
-			Category salad = new Category(name:"Salad", rank:1, owner:normalUser).save(failOnError: true)
-			Category soup = new Category(name:"Soup", rank:1, owner:secondUser).save(failOnError: true)
+				if( !Recipe.count() ){
+					Category mainDish = new Category(name:"Main Dish", rank:2, owner:normalUser).save(failOnError: true)
+					Category salad = new Category(name:"Salad", rank:1, owner:normalUser).save(failOnError: true)
+					Category soup = new Category(name:"Soup", rank:1, owner:secondUser).save(failOnError: true)
 
-			SubCategory beef = new SubCategory(name:"Beef", rank:3, category:mainDish, owner:normalUser).save(failOnError: true)
-			SubCategory chicken = new SubCategory(name:"Chicken", rank:1, category:mainDish, owner:normalUser).save(failOnError: true)
-			SubCategory veggie = new SubCategory(name:"Vegetarian", rank:2, category:mainDish, owner:normalUser).save(failOnError: true)
+					SubCategory beef = new SubCategory(name:"Beef", rank:3, category:mainDish, owner:normalUser).save(failOnError: true)
+					SubCategory chicken = new SubCategory(name:"Chicken", rank:1, category:mainDish, owner:normalUser).save(failOnError: true)
+					SubCategory veggie = new SubCategory(name:"Vegetarian", rank:2, category:mainDish, owner:normalUser).save(failOnError: true)
 
-			SubCategory pasta = new SubCategory(name:"Pasta", rank:2, category:salad, owner:normalUser).save(failOnError: true)
-			SubCategory green = new SubCategory(name:"Green", rank:1, category:salad, owner:normalUser).save(failOnError: true)
+					SubCategory pasta = new SubCategory(name:"Pasta", rank:2, category:salad, owner:normalUser).save(failOnError: true)
+					SubCategory green = new SubCategory(name:"Green", rank:1, category:salad, owner:normalUser).save(failOnError: true)
 
-			Recipe cobbSalad = new Recipe(owner:normalUser, title:"Cobb Salad", body:"Lettuce\nBacon\nBleu", category:salad, subCategory:green).save(failOnError: true)
-			Recipe ceasarSalad = new Recipe(owner:normalUser, title:"Ceasar Salad", body:"Lettuce\nCrouton\nParm", category:salad, subCategory:green).save(failOnError: true)
-			Recipe penneSalad = new Recipe(owner:normalUser, title:"Penne Salad", body:"Penne\nBasil", category:salad, subCategory:pasta).save(failOnError: true)
+					Recipe cobbSalad = new Recipe(owner:normalUser, title:"Cobb Salad", body:"Lettuce\nBacon\nBleu", category:salad, subCategory:green).save(failOnError: true)
+					Recipe ceasarSalad = new Recipe(owner:normalUser, title:"Ceasar Salad", body:"Lettuce\nCrouton\nParm", category:salad, subCategory:green).save(failOnError: true)
+					Recipe penneSalad = new Recipe(owner:normalUser, title:"Penne Salad", body:"Penne\nBasil", category:salad, subCategory:pasta).save(failOnError: true)
 
-			Recipe potRoast = new Recipe(owner:normalUser, title:"Pot Roast", body:"Roast\nCarrots\nPotatoes", category:mainDish, subCategory:beef).save(failOnError: true)
+					Recipe potRoast = new Recipe(owner:normalUser, title:"Pot Roast", body:"Roast\nCarrots\nPotatoes", category:mainDish, subCategory:beef).save(failOnError: true)
 
-			Recipe noSub = new Recipe(owner:normalUser, title:"NoSubRecipe", body:"This\nrecipe\nhas\nno\nSubCategory", category:mainDish).save(failOnError: true)
-			Recipe largeRecipe = new Recipe(owner:normalUser, title:"Latkes- Sweet Potato-Parsnip", body:"Prep and Cook Time: 1 hour, 10 minutes. Notes: See \"Tips for Terrific Latkes,\" below. \r\n" +
+					Recipe noSub = new Recipe(owner:normalUser, title:"NoSubRecipe", body:"This\nrecipe\nhas\nno\nSubCategory", category:mainDish).save(failOnError: true)
+					Recipe largeRecipe = new Recipe(owner:normalUser, title:"Latkes- Sweet Potato-Parsnip", body:"Prep and Cook Time: 1 hour, 10 minutes. Notes: See \"Tips for Terrific Latkes,\" below. \r\n" +
 					"\r\n" +
 					"\r\n" +
 					"2 pounds Garnet sweet potatoes (often labeled \"yams\"), rinsed and peeled \r\n" +
@@ -97,8 +122,10 @@ class BootStrap {
 					"CALORIES 123 (39% from fat); FAT 5.3g (sat 0.9g); PROTEIN 2.7g; CHOLESTEROL 51mg; SODIUM 256mg; FIBER 2.1g; CARBOHYDRATE 16g \r\n" +
 					"\r\n" +
 					"Sunset, DECEMBER 2006", category:mainDish).save(failOnError: true)
+				}
+			}
 		}
-	}
-	def destroy = {
+		def destroy = {
+		}
 	}
 }
