46126eb93885c6324810d55bfb9924b183a93bf1
danebert <danebert@users.noreply.github.com>
2012-04-09 23:29:01 +0000

    Updating to Grails version 2.0.3

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -14,7 +14,7 @@ environments {
 	development {
 		dataSource {
 			dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-			url = "jdbc:h2:mem:devDb;MVCC=TRUE;AUTO_SERVER=TRUE"
+			url = "jdbc:h2:mem:devDb;MVCC=TRUE"
 		}
 	}
 	test {
@@ -27,17 +27,6 @@ environments {
 		dataSource {
 			dbCreate = "update"
 			url = "jdbc:h2:/home/recipes/recipesDb;MVCC=TRUE;AUTO_SERVER=TRUE"
-			// For MySQL production scenarios enable the following settings
-			//          pooled = true
-			//          properties {
-			//               minEvictableIdleTimeMillis=1800000
-			//               timeBetweenEvictionRunsMillis=1800000
-			//               numTestsPerEvictionRun=3
-			//               testOnBorrow=true
-			//               testWhileIdle=true
-			//               testOnReturn=true
-			//               validationQuery="SELECT 1"
-			//          }
 		}
 	}
 }
