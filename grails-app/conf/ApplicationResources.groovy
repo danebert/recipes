11f27a9328d91f6fdf8c71e7d5535eeb951cc00b
danebert <danebert@users.noreply.github.com>
2014-02-04 16:12:01 +0000

    Update to make drag and drop sorting work on iPad/iPhone

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,5 +1,4 @@
 modules = {
-    application {
-        resource url:'js/application.js'
-    }
+	application { resource url:'js/application.js' }
+	touchPunch { resource url: 'js/jquery.ui.touch-punch.min.js' }
 }
